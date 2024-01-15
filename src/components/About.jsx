888c20a4b613cab7b4493c948cb03dac5c93a6ce
tulobaev <talgattulobaev519>
2024-01-15 16:59:36 +0600

    about page

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components/About.css";
+
+const About = () => {
+  return (
+    <div>
+      <h1>My name is Talgat</h1>
+    </div>
+  );
+};
+
+export default About;
